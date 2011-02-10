15c15f8d717be02c978ccb6e50e943a0f67d44f5
Eric <eric.c.chi@gmail.com>
2011-02-10 10:40:34 -0600

    Adding R wrapper for logisticL2Eas (version with active sets) and logisticL2E.R now uses active Sets

--- R/logisticL2E.R
+++ R/logisticL2E.R
@@ -1,11 +1,6 @@
 soft.th = function(x,lambda) {
 	n = length(x)
-	y = matrix(0,nrow=n)
-	posIX = which(x > lambda)
-	y[posIX] = x[posIX] - lambda
-	negIX = which(x < -lambda)
-	y[negIX] = x[negIX] + lambda
-	return(y)
+	return(sign(x) * max(abs(x) - lambda, 0))
 }
 
 # X's first column does not have ones.
@@ -21,10 +16,15 @@ cdL2E = function(X, Y, alpha, lambda, beta0, beta, w = 1, K = 0.16, niterInner =
 	Ki = 1/K;
 	U = 2*Y-1
 	S = apply(X,2,FUN = function(x) {return(t(x)%*%x)} )
-	m = w*K/n
+#-	m = w*K/n #-
+	m = 1/n   #+
 	mS = m * S
+	lambda = lambda/(w*K) #+
 
 	betaHx = c(beta0, beta)
+
+	activeSet = 1:p
+	inActiveSet = setdiff(1:p, activeSet)
 
 	for (t in 1:niterOuter) {
 		lastOuter = c(beta0, beta)
@@ -33,25 +33,63 @@ cdL2E = function(X, Y, alpha, lambda, beta0, beta, w = 1, K = 0.16, niterInner =
 		D = 2*P - 1
 		WZ = P * (1-P) * (w * D - U)
 		zeta = matrix(beta0 + Xbeta - Ki*WZ, ncol=1)
-		for (iter in 1:niterInner) {
-			lastInner = c(beta0, beta)
-			# Update beta0 
-			beta0 = mean(zeta - X %*% beta)
-			# Update beta
-			for (j in seq(p,1,-1)) {
-				mask = matrix(1, p, 1)
-				mask[j] = 0
-				pResponse = beta0 + X %*% (mask * beta)
-				pResidual = zeta - pResponse
-				v = soft.th(m * t(X[,j,drop=F]) %*% pResidual, lambda*alpha)
-				v = v / (mS[j] + lambda*(1-alpha))
-				beta[j] = v
+
+#+		beta0 = mean(zeta)  				#+
+#+		zeta = zeta - beta0 				#+
+		repeat {
+			print("Active Set:")
+			print(activeSet)
+			print("Inactive Set:")
+			print(inActiveSet)
+			if (length(activeSet) > 0) {
+				Xa = X[, activeSet, drop=F]
+				betaActive = beta[activeSet]
+				for (iter in 1:niterInner) {
+					lastInner = c(beta0, betaActive)
+					# Update beta0 
+					beta0 = mean(zeta - Xa %*% betaActive)        #-
+					# Update beta
+					for (j in 1:length(activeSet)) {
+						pResponse = beta0 + Xa[,-j,drop=F] %*% betaActive[-j] #-
+#+						pResponse = Xa[,-j,drop=F] %*% betaActive[-j]         #+
+						pResidual = zeta - pResponse
+						v = soft.th(m * t(Xa[,j,drop=F]) %*% pResidual, lambda*alpha)
+						v = v / (mS[activeSet[j]] + lambda*(1-alpha))
+						betaActive[j] = v
+					}
+					# Check inner loop convergence
+					if (sqrt(sum(c(beta0, betaActive) - lastInner)^2) < tol) {
+						print(paste("Number of inner iterations = ", iter))
+						break
+					}
+				}
+				beta[activeSet] = betaActive
 			}
-			# Check inner loop convergence
-			if (sqrt(sum(c(beta0,beta) - lastInner)^2) < tol) {
-				print(paste("Number of inner iterations = ", iter))
+
+			pResponse = beta0 + X %*% beta                #-
+#+			pResponse = Xa[,-j,drop=F] %*% betaActive[-j] #+
+			pResidual = zeta - pResponse
+			for (j in 1:length(inActiveSet)) {
+				v = soft.th(m * t(X[,inActiveSet[j],drop=F]) %*% pResidual, lambda*alpha)
+				v = v / (mS[inActiveSet[j]] + lambda*(1-alpha))
+				beta[inActiveSet[j]] = v
+			}
+			# Remove variables from the active set
+			deActiveSet = activeSet[which(abs(betaActive) < 1e-16)]
+			activeSet = setdiff(activeSet, deActiveSet)
+			# Add variables to inactive set
+			inActiveSet = union(inActiveSet, deActiveSet)
+			# Check inactive variables if they need to be added.
+			addActiveSet = inActiveSet[which(abs(beta[inActiveSet]) > 0)]
+			print(addActiveSet)
+			if (length(addActiveSet) == 0)
 				break
-			}
+			# Remove variables from inActiveSet
+			inActiveSet = setdiff(inActiveSet, addActiveSet)
+			# Add variables to activeSet
+			activeSet = union(activeSet, addActiveSet)
+			if (length(activeSet) == 0)
+				break
 		}
 		betaHx = cbind(betaHx, c(beta0, beta))
 		# Check outer loop convergence
